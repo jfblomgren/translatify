c69090efb265e9cc0c99c021bf7a9fddc46b19a3
Encrylize <encrylize@gmail.com>
2017-08-11 16:49:26 +0200

    Move everything into functions

--- translatify.py
+++ translatify.py
@@ -34,36 +34,49 @@ def language_list(string):
             lang_list.append(language(lang, allow_auto=False))
     return lang_list
 
+def setup_parser(parser):
+    parser.add_argument('-b', '--brief', help='only show the final result',
+                        action='store_true')
+    parser.add_argument('-s', '--source', metavar='LANGUAGE',
+                        help='language of the original text (default: auto)',
+                        type=language, default='auto')
+    parser.add_argument('-t', '--targets', metavar='LANGUAGES',
+                        help='ordered, comma-separated list of languages to '
+                             'translate the text to (default: all)',
+                        type=language_list, default='all')
+    parser.add_argument('text', metavar='TEXT', help='the text to translate',
+                        type=str)
 
-translator = Translator()
-parser = argparse.ArgumentParser()
-parser.add_argument('-b', '--brief', help='only show the final result',
-                    action='store_true')
-parser.add_argument('-s', '--source', metavar='LANGUAGE',
-                    help='the language of the original text (default: auto)',
-                    type=language, default='auto')
-parser.add_argument('-t', '--targets', metavar='LANGUAGES',
-                    help='an ordered, comma-separated list of languages to '
-                         'translate the text to (default: all)',
-                    type=language_list, default='all')
-parser.add_argument('text', metavar='TEXT', help='the text to translate',
-                    type=str)
-args = parser.parse_args()
+def translatify_text(text, source, targets, show_steps):
+    translator = Translator()
 
-for i, target in enumerate(args.targets):
-    source = args.source if i == 0 else args.targets[i - 1]
-    translation = translator.translate(args.text, src=source, dest=target)
-    args.text = translation.text
+    for i, target in enumerate(targets):
+        if i != 0:
+            source = targets[i - 1]
 
-    if args.brief:
-        continue
+        translation = translator.translate(text, src=source, dest=target)
+        text = translation.text
 
-    # If the original source was 'auto', the source will be automatically
-    # detected during translation.
-    source = translation.src.lower()
-    print('[{} -> {}]'.format(CODE_TO_LANG[source].title(),
-                              CODE_TO_LANG[target].title()))
-    print(args.text)
+        if not show_steps:
+            continue
 
-if args.brief:
-    print(args.text)
+        # If the original source was 'auto', the source will be automatically
+        # detected during translation.
+        source = translation.src.lower()
+        print('[{} -> {}]'.format(CODE_TO_LANG[source].title(),
+                                  CODE_TO_LANG[target].title()))
+        print(text)
+
+    if not show_steps:
+        print(text)
+
+def main():
+    parser = argparse.ArgumentParser()
+    setup_parser(parser)
+    args = parser.parse_args()
+
+    translatify_text(args.text, args.source, args.targets, not args.brief)
+
+
+if __name__ == '__main__':
+    main()
